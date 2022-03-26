e83fe36ac3c2cbaf38a5ae070ae39f55c6fb2df4
surajeet310 <surajeet310@gmail.com>
2022-03-27 01:49:34 +0530

    Added Workspace endpoint handlers and their tests

--- api/src/main/java/com/herokuapp/serverbugit/api/services/BugitAuthApiServices.kt
+++ api/src/main/java/com/herokuapp/serverbugit/api/services/BugitAuthApiServices.kt
@@ -1,6 +1,68 @@
 package com.herokuapp.serverbugit.api.services
 
+import com.herokuapp.serverbugit.api.models.GlobalServerResponse
+import com.herokuapp.serverbugit.api.models.workspaces.*
+import retrofit2.Response
+import retrofit2.http.*
+import java.util.*
+
 
 interface BugitAuthApiServices {
 
+    //TODO Workspace Endpoints
+
+    @GET("/auth/home")
+    suspend fun getWorkspaces(
+        @Query("user_id") userId:UUID
+    ):Response<HomeResponse>
+
+    @GET("/auth/home/workspace")
+    suspend fun getSingleWorkspace(
+        @Query("workspace_id") workspaceId:UUID
+    ):Response<SingleWorkspaceResponse>
+
+    @GET("/auth/workspaceMembers")
+    suspend fun getWorkspaceMembers(
+        @Query("workspace_id") workspaceId: UUID, @Query("project_id") projectId:UUID
+    ):Response<WorkspaceMemberResponse>
+
+    @GET("/auth/allWorkspaceMembers")
+    suspend fun getAllWorkspaceMembers(
+        @Query("workspace_id") workspaceId: UUID
+    ):Response<WorkspaceMemberResponse>
+
+    @GET("/auth/requests")
+    suspend fun getRequests(
+        @Query("user_id") userId:UUID
+    ):Response<RequestResponse>
+
+    @POST("/auth/addWorkspace")
+    suspend fun addWorkspace(
+        @Body workspace:AddWorkspace
+    ):Response<GlobalServerResponse>
+
+    @POST("/auth/addWorkspaceMemberReq")
+    suspend fun addWorkspaceMemberReq(
+        @Body workspaceMemberRequest:AddWorkspaceMemberRequest
+    ):Response<GlobalServerResponse>
+
+    @POST("/auth/addWorkspaceMember")
+    suspend fun addWorkspaceMember(
+        @Body workspaceMember:AddWorkspaceMember
+    ):Response<GlobalServerResponse>
+
+    @POST("/auth/makeUserAdmin")
+    suspend fun makeUserAdmin(
+        @Body user:MakeUserAdmin
+    ):Response<GlobalServerResponse>
+
+    @DELETE("/auth/deleteWorkspace")
+    suspend fun deleteWorkspace(
+        @Query("workspace_id") workspaceId: UUID
+    ):Response<GlobalServerResponse>
+
+    @DELETE("/auth/removeWorkspaceMember")
+    suspend fun removeWorkspaceMember(
+        @Query("workspace_id") workspaceId: UUID, @Query("user_id") userId:UUID
+    ):Response<GlobalServerResponse>
 }
--- api/src/test/java/com/herokuapp/serverbugit/api/BugitAuthApiTests.kt
+++ api/src/test/java/com/herokuapp/serverbugit/api/BugitAuthApiTests.kt
@@ -1,11 +1,124 @@
 package com.herokuapp.serverbugit.api
 
 import com.herokuapp.serverbugit.api.models.workspaces.AddWorkspace
+import com.herokuapp.serverbugit.api.models.workspaces.AddWorkspaceMember
+import com.herokuapp.serverbugit.api.models.workspaces.AddWorkspaceMemberRequest
+import com.herokuapp.serverbugit.api.models.workspaces.MakeUserAdmin
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.Assert.*
 import java.util.*
 
 class BugitAuthApiTests {
+    val api = BugitClient.getAuthApiInstance()
 
+    // TODO Workspaces
+
+    @Test
+    fun `Add Workspace`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        val userId = "3d21cef2-84ae-48f4-96d5-5b7b6de91351"
+        val name = "W 2"
+        val desc = "bdvhjbvhjdbshb"
+        val createdAt = "2022-03-26"
+         runBlocking {
+             val res = api.addWorkspace(AddWorkspace(UUID.fromString(userId),name,desc, createdAt))
+             assertEquals("success",res.body()?.response)
+         }
+    }
+
+    @Test
+    fun `Add Workspace Member Request`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        val request = AddWorkspaceMemberRequest(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"),"bruce.wayne330@test.com",
+            UUID.fromString("3d21cef2-84ae-48f4-96d5-5b7b6de91351"))
+        runBlocking {
+            val res = api.addWorkspaceMemberReq(request)
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Add Workspace Member`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.addWorkspaceMember(AddWorkspaceMember(UUID.fromString("63cfc8e4-4789-4aed-983d-5f8563576eb0")))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Workspace Member Exists`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        val request = AddWorkspaceMemberRequest(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"),"john.snow@abc.com",
+            UUID.fromString("3d21cef2-84ae-48f4-96d5-5b7b6de91351"))
+        runBlocking {
+            val res = api.addWorkspaceMemberReq(request)
+            assertEquals("exists",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Make User Admin`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.makeUserAdmin(MakeUserAdmin(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"), UUID.fromString("bcaf8345-ed6c-49af-963e-9138e397180e")))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Get Home`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.getWorkspaces(UUID.fromString("3d21cef2-84ae-48f4-96d5-5b7b6de91351"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Get Single Workspace`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.getSingleWorkspace(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Get All Workspace Members`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.getAllWorkspaceMembers(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Delete Workspace`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.deleteWorkspace(UUID.fromString("3aa51949-d3c6-425e-81e6-14afcab8e015"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Remove Workspace Member`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.removeWorkspaceMember(UUID.fromString("c439d52d-b5e9-497d-86a2-ede99848f140"),
+                UUID.fromString("bcaf8345-ed6c-49af-963e-9138e397180e"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
+
+    @Test
+    fun `Get Requests`(){
+        BugitClient.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NjM2NjE0NzcsInVzZXJfaWQiOiIzZDIxY2VmMi04NGFlLTQ4ZjQtOTZkNS01YjdiNmRlOTEzNTEifQ.3oqluK5_SCk2E1L0gBoKpyMroicvlL95u1cmEqi6Ma8"
+        runBlocking {
+            val res = api.getRequests(UUID.fromString("bcaf8345-ed6c-49af-963e-9138e397180e"))
+            assertEquals("success",res.body()?.response)
+        }
+    }
 }
